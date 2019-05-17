e2981c89506b80c40ccd41e41b3cde64c113abbc
Merline Belizaire <merline@noukod.com>
2019-05-17 19:03:34 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,32 @@
+<<<<<<< HEAD
 class SessionsController < ApplicationController::Base
   def create
     @user = User.find_by(username: params[:username])
     return head(:forbidden) unless @user.authenticate(params[:password])
     session[:user_id] = @user.id
+=======
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(name: params[:user][:name])
+
+    user = user.try(:authenticate, params[:user][:password])
+
+    return redirect_to(controller: 'sessions', action: 'new') unless user
+
+    session[:user_id] = user.id
+
+    @user = user
+
+    redirect_to controller: 'welcome', action: 'home'
+  end
+
+  def destroy
+    session.delete :user_id
+
+    redirect_to '/'
+>>>>>>> e385d031dc3568625c075a3b9c0739a1ac6133f8
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 class WelcomeController < ApplicationController::Base
+=======
+class WelcomeController < ApplicationController
+>>>>>>> e385d031dc3568625c075a3b9c0739a1ac6133f8
   before_action :require_logged_in
 
   def home
